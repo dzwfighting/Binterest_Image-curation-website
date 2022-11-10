0f1648b4b748de2aaf7ace205ffa70e3cbc2231c
dzwfighting <yiwen7943@gmail.com>
2022-11-10 14:09:44 -0500

    Update Navigation.jsx

--- client/src/component/Navigation.jsx
+++ client/src/component/Navigation.jsx
@@ -3,11 +3,40 @@ import { Link } from "react-router-dom";
 import "../App.css";
 import "../style/navigation.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const Navigation = () => {
   return (
-    <div>
-      <div className="link_margin">
+    <div className="hei">
+      <Navbar className="bg" expand="lg">
+        <Container>
+          {/* <Navbar.Brand href="/">All Images List</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/" className="dis">
+                All Images List
+              </Nav.Link>
+              <Nav.Link href="/my-bin" className="dis">
+                Bin
+              </Nav.Link>
+              <Nav.Link href="/my-posts" className="dis">
+                All Upload images
+              </Nav.Link>
+              <Nav.Link href="/new-post" className="dis">
+                Upload posts
+              </Nav.Link>
+              <Nav.Link href="/popularity" className="dis">
+                Popularity
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* <div className="link_margin">
         <Link className="App-link btn btn-outline-success" to="/">
           All Images List
         </Link>
@@ -31,7 +60,7 @@ const Navigation = () => {
         <Link className="App-link btn btn-outline-success" to="/popularity">
           Popularity
         </Link>
-      </div>
+      </div> */}
     </div>
   );
 };
